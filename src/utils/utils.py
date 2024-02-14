cc26f1c104255e12c07d83546762a39864c4efc3
Amankd777 <20bds003@iiitdwd.ac.in>
2024-02-14 21:52:18 +0530

    Components setup done

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -52,4 +52,4 @@ def load_object(file_path):
             return pickle.load(file_obj)
     except Exception as e:
         logging.info('Exception Occured in load_object function utils')
-        raise customexception(e,sys)
+        raise CustomException(e,sys)
